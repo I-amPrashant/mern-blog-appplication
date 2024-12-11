dfd39b23b556f144775d54cfbea245cb2a69e55c
I-amPrashant <airmax50cent@gmail.com>
2024-12-11 08:27:19 +0545

    add redux persist to persist currentUser

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,29 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {configureStore, combineReducers} from '@reduxjs/toolkit'
 import userReducer from '../user/userSlice.js'
+import {persistReducer} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import persistStore from 'redux-persist/es/persistStore'
+import {createFilter} from 'redux-persist-transform-filter'
+
+const rootReducer=combineReducers({
+    user:userReducer,
+})
+
+const filterUser=createFilter('user', ['currentUser']);
+
+const persistConfig={
+    key:'root',
+    storage,
+    version:1,
+    whitelist:['user'],
+    transforms:[filterUser],
+}
+
+const persistedReducer=persistReducer(persistConfig, rootReducer)
 
 export const store=configureStore({
-    reducer:{
-        user:userReducer,
-    },
+    reducer:persistedReducer,
+    middleware:(getDefaultMiddleware)=>getDefaultMiddleware({serializableCheck:false}),
 })
+
+export const persistor=persistStore(store);
